fc78fb9b3fff88a4497ef4ec801f01f3da09cefd
OSMAN EMİR <oe8001@outlook.com>
2023-12-30 00:41:31 +0300

    WriteRepository

--- Infrastructure/Monster.Persistence/Registration.cs
+++ Infrastructure/Monster.Persistence/Registration.cs
@@ -20,6 +20,7 @@ namespace Monster.Persistence
             services.AddDbContext<ApiDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 
         }
     }
