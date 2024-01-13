b9de16701ac956d32e96aefcf2c260e86e8def26
OSMAN EMİR <oe8001@outlook.com>
2024-01-14 02:30:05 +0300

    JWT Token Service

--- Infrastructure/Monster.Infrastructure/Tokens/TokenService.cs
+++ Infrastructure/Monster.Infrastructure/Tokens/TokenService.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,42 +26,67 @@ namespace Monster.Infrastructure.Tokens
         }
         public async Task<JwtSecurityToken> CreateToken(User user, IList<string> roles)
         {
+            // JWT içinde yer alacak talepleri temsil eden bir liste oluşturulur
             var claims = new List<Claim>()
             {
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID (Jti) talebi eklenir.
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), // Kullanıcının benzersiz kimlik numarası talebi eklenir.
+                new Claim(JwtRegisteredClaimNames.Email, user.Email) // Kullanıcının e-posta talebi eklenir.
             };
-
+            // Kullanıcının sahip olduğu roller eklenir.
             foreach (var role in roles)
             {
                 claims.Add(new Claim(ClaimTypes.Role, role));
             }
 
+            // JWT için kullanılacak simetrik anahtar oluşturulur.
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret));
-
+            //JWT oluşturur
             var token = new JwtSecurityToken(
-                issuer: tokenSettings.Issuer,
-                audience: tokenSettings.Audience,
-                expires: DateTime.Now.AddMinutes(tokenSettings.TokenValidityInMunitues),
-                claims: claims,
-                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                issuer: tokenSettings.Issuer,// JWT'nin düzenleyeni (issuer) belirlenir.
+                audience: tokenSettings.Audience,// JWT'nin kullanılacağı hedef kitle (audience) belirlenir.
+                expires: DateTime.Now.AddMinutes(tokenSettings.TokenValidityInMunitues),// JWT'nin geçerlilik süresi belirlenir.
+                claims: claims,// JWT içinde taşınacak talepler belirlenir
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)// JWT'nin imzalanması için gerekli bilgiler belirlenir.
                 );
 
+            // Kullanıcıya ait talepler veritabanına eklenir.
             await userManager.AddClaimsAsync(user, claims);
-
+            //JWT döndürür
             return token;
 
         }
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
-        public ClaimsPrincipal? GetPrincipalFromExpiredToken()
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters tokenValidationParamaters = new()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret)),
+                ValidateLifetime = false
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParamaters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken
+                || !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Token bulunamadı.");
+
+            return principal;
+
         }
     }
 }
--- Presentation/Monster.Api/Program.cs
+++ Presentation/Monster.Api/Program.cs
@@ -3,6 +3,7 @@ using Monster.Application;
 using Monster.Infrastructure;
 using Monster.Mapper;
 using Monster.Application.Exceptions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,35 @@ builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Monster API", Version = "v1", Description = "Monster API swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazip bosluk biraktiktan sonra Token'i Girebilirsiniz \r\n\r\n ornegin: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+
+    });
+});
 
 
 var app = builder.Build();
